5371bb226323fe84186b1db05c941caefe16b75f
Bermlida <fir4268@gmail.com>
2016-09-02 16:43:10 +0800

    router dev init

---
+++ src/Router/UserSettings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Vista\Router;
+
+
+class Router extends Model
+{
+
+    
+}
+
+/* End of file UserSetting.php */
+/* Location: .//home/tkb-user/projects/laravel/app/Repositories/Entities/UserSetting.php */
