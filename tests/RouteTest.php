5b5688af1f867bda6b167594d3a04a823d543c50
Bermlida <fir4268@gmail.com>
2016-09-29 01:14:12 +0800

    Route Test Case

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -5,24 +5,78 @@ use Vista\Router\Route;
 
 class RouteTest extends PHPUnit_Framework_TestCase
 {
-    public function additionProvider()
+    public function routeProvider()
     {
         return [
             [new Route()]
         ];
     }
 
-    public function testName(RouteInterface $route)
+    /**
+     * @dataProvider routeProvider
+     */
+    public function testSetRoute(RouteInterface $route)
     {
-        $route->name_prefix("user")->name("accounts");
+        $route
+            ->name_prefix('users.account.')->name('.profiles.item')
+            ->path_prefix('/users/{user_id}/account/')->path('/pofiles/{item_name}/{item_prototype}')
+            ->tokens('user_id', '\d+')->tokens(['item_prototype' => 'name|value'])
+            ->methods('get')->methods(['options', 'header']);
 
-        $this->assertEquals($this->name_prefix, "user");
-        $this->assertEquals($this->name, "accounts");
-        $this->assertEquals($this->full_name, "user.accounts");
+        return $route;
     }
 
+    /**
+     * @depends testName
+     */
     public function testPath()
     {
-        $this->assertEquals(2, 2);
+        $route->path_prefix('/users/')->path('{user_id}/account/{profiles_item}');
+
+        $this->assertEquals($this->name_prefix, 'users');
+        $this->assertEquals($this->name, 'account.profiles');
+        $this->assertEquals($this->full_name, 'users.account.profiles');
+
+        $this->assertEquals($this->path_prefix, 'users');
+        $this->assertEquals($this->path, '{user_id}/account/{profiles_item}');
+        $this->assertEquals($this->full_path, 'users/{user_id}/account/{profiles_item}');
+
+        return $route;
+    }
+
+    /**
+     * @depends testPath
+     */
+    public function testRegex()
+    {
+        $route->tokens('user_id', '\d+');
+
+        $this->assertEquals($this->full_regex, "users/(\d+)/account/('\w+)");
+
+        return $route;
+    }
+
+    /**
+     * @depends testMethod
+     */
+    public function testMethod()
+    {
+        $route
+
+        $this->assertEquals($this->methods, ["GET", "OPTIONS", "HEADER"]);
+
+        return $route;
+    }
+
+    /**
+     * @depends testMethod
+     */
+    public function testMethod()
+    {
+        $route->methods('get')->methods(['options', 'header']);
+
+        $this->assertEquals($this->methods, ["GET", "OPTIONS", "HEADER"]);
+
+        return $route;
     }
 }
