52cf5804fbc4ca4fcfe9f424fab3fe57cc6b0b3a
Bermlida <fir4268@gmail.com>
2016-10-06 17:52:04 +0800

    Write a test case for RouteCollectionTrait 1.write a test case for RouteCollectionTrait
    2.fix RouteCollectionTrait

--- src/Router/Traits/RouteCollectionTrait.php
+++ src/Router/Traits/RouteCollectionTrait.php
@@ -9,6 +9,8 @@ trait RouteCollectionTrait
 {
     public function setRoutes(array $routes)
     {
+        $this->routes = [];
+
         array_walk($routes, [$this, "setRoute"]);
     }
 
@@ -29,7 +31,7 @@ trait RouteCollectionTrait
         foreach ($this->routes as $current_route) {
             $compare_path = $current_route->full_path == $route->full_path;
             $compare_methods = !empty(array_intersect($route->methods, $current_route->methods));
-            $compare_name = ($current_route->full_name == $route->full_name) && !$full_name_empty;
+            $compare_name = ($current_route->full_name == $route->full_name) && !$name_is_empty;
 
             if ($compare_path && $compare_methods && $compare_name) {
                 throw new RuntimeException('');
@@ -49,7 +51,7 @@ trait RouteCollectionTrait
     public function getRoute(string $name, $methods = null)
     {
         if (is_string($methods) || is_array($methods)) {
-            if (($key = $this->searchRoute($name, $methods)) > 0) {
+            if (($key = $this->searchRoute($name, $methods)) >= 0) {
                 return $this->routes[$key];
             }
         } elseif (isset($this->routes[$name])) {    
@@ -66,7 +68,7 @@ trait RouteCollectionTrait
             $key = $name;
         }
 
-        if (isset($key) && $key > 0) {
+        if (isset($key) && $key >= 0) {
             unset($this->routes[$key]);
         } else {
             throw new RuntimeException('');
@@ -77,6 +79,7 @@ trait RouteCollectionTrait
     {
         if (is_string($methods) || is_array($methods)) {
             $methods = is_string($methods) ? [$methods] : $methods;
+            $path = trim($path, '/');
             
             foreach ($this->routes as $key => $route) {
                 $compare_path = $route->full_path == $path;
---
+++ tests/RouteCollectionTraitTest.php
@@ -0,0 +1,117 @@
+<?php
+
+use Vista\Router\Traits\RouteCollectionTrait;
+use Vista\Router\Route;
+
+class RouteCollectionTraitTest extends PHPUnit_Framework_TestCase
+{
+    private $route;
+
+    public function setUp()
+    {
+        $route = (new Route())
+                            ->name_prefix('users.account.')->name('settings')
+                            ->path_prefix('/users/{user_id}/account/')->path('/settings/{item}')
+                            ->methods('get')->methods(['put', 'delete']);
+
+        $this->route = $route;
+    }
+
+    public function testStub()
+    {
+        $stub = $this->getMockForTrait(RouteCollectionTrait::class);
+
+        $stub->routes = [];
+
+        return $stub;
+    }
+
+    /**
+     * @depends clone testStub
+     */
+    public function testSetRoute($stub)
+    {
+        $stub->setRoute($this->route);
+
+        return $stub;
+    }
+
+    /**
+     * @depends clone testStub
+     */
+    public function testSetRoutes($stub)
+    {
+        $stub->setRoutes([$this->route]);
+
+        return $stub;
+    }
+
+    /**
+     * @depends testSetRoutes
+     */
+    public function testGetRoutes($stub)
+    {
+        $this->assertEquals($stub->getRoutes(), [$this->route]);
+    }
+
+    /**
+     * @depends testSetRoutes
+     */
+    public function testRemoveRoutes($stub)
+    {
+        $stub->removeRoutes();
+
+        $this->assertEquals($stub->getRoutes(), []);
+    }
+
+    /**
+     * @depends testSetRoute
+     */
+    public function testGetRoute($stub)
+    {
+        $this->assertEquals($stub->getRoute(0), $this->route);
+
+        $this->assertEquals(
+            $stub->getRoute('/users/{user_id}/account/settings/{item}', 'put'),
+            $this->route
+        );
+
+        $this->assertEquals(
+            $stub->getRoute(
+                'users/{user_id}/account/settings/{item}/',
+                ['delete', 'put']
+            ),
+            $this->route
+        );
+    }
+
+    /**
+     * @depends testSetRoute
+     */
+    public function testRemoveRoute($stub)
+    {
+        (clone $stub)->removeRoute(0);
+
+        (clone $stub)->removeRoute('/users/{user_id}/account/settings/{item}/', 'get');
+
+        (clone $stub)->removeRoute('users/{user_id}/account/settings/{item}', ['get', 'delete']);
+    }
+    
+    /**
+     * @depends testSetRoute
+     */
+    public function testSearchRoute($stub)
+    {
+        $this->assertEquals($stub->searchRoute('users/{user_id}/account/settings/{item}/', ['delete', 'put']), 0);
+
+        $this->assertEquals($stub->searchRoute('/users/{user_id}/account/settings/{item}', 'get'), 0);
+
+        $this->assertEquals($stub->searchRoute('/users/{user_id}/account/settings/{item}/', 'options'), -1);
+
+        $this->assertEquals($stub->searchRoute('users/{user_id}/account/settings/{item}', 'header'), -1);
+
+        $this->assertEquals($stub->searchRoute('users/{user_id}/account/profiles', 'get'), -1);
+
+        $this->assertEquals($stub->searchRoute('/users/{user_id}/account/profiles/', ['get', 'put', 'delete']), -1);
+    }
+}
