6296f90bce2f3f13fe4e49c411214b6e16d5fba8
Bermlida <fir4268@gmail.com>
2016-10-07 12:34:53 +0800

    Write test case for RouteSetterTrait

--- tests/RouteGetterTraitTest.php
+++ tests/RouteGetterTraitTest.php
@@ -5,16 +5,14 @@
 */	
 class RouteGetterTraitTest extends PHPUnit_Framework_TestCase
 {
-	// private $button = NULL;
-	// private $form = NULL;
-	// private $navigation_bar = '';
-	// private $button_html = '';
-	// private $form_html = '';
-	// private $page_script = '';
-    
+	// private NULL 
+	// private private 
+	// private 
+	// private  
+	// private 
+/*
 	public function testStub()
     {
-		
 	}
 
 	public function testNamePrefix()
@@ -22,19 +20,16 @@ class RouteGetterTraitTest extends PHPUnit_Framework_TestCase
         //
 	}
 	
-	public function testPathPrefix($id , $title , $extra = '' , $show = TRUE , $redirect = FALSE , $link = '')
+	public function testPathPrefix($id , $title , $extra $show $link)
     {
-        //
     }
 	
-	public function testName($button_name , $script_type , $script_param = '')
+	public function testName($button_name , $script_type)
     {
-        //
 	}
 	
-	public function testPath($id , $title , $extra = '')
+	public function testPath(  )
     {
-        //
 	}
 
     public function testTokens()
@@ -42,38 +37,30 @@ class RouteGetterTraitTest extends PHPUnit_Framework_TestCase
         //
     }
 
-	public function testFullName($form_name , $extra_name , $extra_value)
+	public function testFullName($form_name , $extra_name $extra_value)
     {
         //
 	}
 
-	public function testFullPath($form_name , $content_type , $content_value)
+	public function testFullPath($form_name , $content_type $content_value)
     {
         //
 	}
 	
-	public function testFullRegex()
+	public function testFullRegex( $param)
     {
         //
 	}
-	
-    /**
-     * 
-     */
+    
 	public function testMethods()
     {
-        //
 	}
 
-    /**
-     * 
-     */
-    public function testHandler($page_part)
+    public function testHandler()
     {
-        //
     }
 
-	public function testParamSources($acttype , $helpkey)
+	public function testParamSources()
     {
         //
 	}
@@ -82,32 +69,16 @@ class RouteGetterTraitTest extends PHPUnit_Framework_TestCase
     {
 	}
 
-	public function testHandlerResolve($acttype)
+	public function testHandlerResolve()
     {
 	}
 
-	public function testParamHandlersResolve($acttype)
+	public function testParamHandlersResolve()
     {
 	}
-	
-	private function setStubMethods($acttype)
-    {
-	}
-	
-	private function setStubProperty($acttype , $link)
-    {
-	}
-/*
-    public function get_redirect_script($selector , $link , $method , $param)
-    {
-	}		
-  
-	public function get_lightbox_script($selecter , $link , $param)
-    {
-	}
-	
-	public function get_default_script($acttype , $sub_script)
-    {
-	}
+
 */
+    /**
+     * 
+     */
 }
