5853c8c118b9178ec8b5725db7198490575e5c6e
Bermlida <fir4268@gmail.com>
2016-10-19 15:54:41 +0800

    modify test case for RouteTrait and test

--- tests/RouteTraitTest.php
+++ tests/RouteTraitTest.php
@@ -24,30 +24,20 @@ class RouteTraitTest extends PHPUnit_Framework_TestCase
         $stub->full_regex = 'users\/(\d+)\/account\/profiles\/(\w+)\/(name|value)';
         $stub->methods = ['get', 'options', 'header'];
 
-        $stub->param_sources = [
-            'sort' => 'get',
-            'top' => 'post'
-        ];
         $stub->param_handlers = [
+            'user' => function ($param) {
+                return (object)['user_id' => $param];
+            },
             'sort' => function ($param) {
                 return $param * 10;
             },
             'top' => function ($param) {
-                return $param / 10;
+                return round($param / 10, 1);
             }
         ];
-        $stub->handler = function ($sort, $top) {
-            if ($sort > 100 && $top < 10) {
-                return 'correct value';
-            } else {
-                return 'error value';
-            }
-        };
 
         $this->setResolveHandlerMethod($stub);
-        $this->setResolveSourcesMethod($stub);
         $this->setHandleParams($stub);
-        $this->setBindArguments($stub);
         $this->setCallHandler($stub);
 
         return $stub;
@@ -73,54 +63,111 @@ class RouteTraitTest extends PHPUnit_Framework_TestCase
     
     /**
      * @dataProvider  requestProvider
-     * @depends testStub
+     * @depends clone testStub
      */
-    public function testExecuteHandler($request , $stub)
+    public function testExecuteHandlerWithParams($request , $stub)
     {
+        $stub->param_sources = [
+            'sort' => 'get',
+            'top' => 'post'
+        ];
+
+        $stub->handler = function ($sort, $top) {
+            if ($sort == 220 && $top == 3.3) {
+                return 'correct value';
+            } else {
+                return 'error value with params';
+            }
+        };
+
+        $stub->method('resolveSources')->will(
+            $this->returnCallback(function ($request) {
+                return [
+                    'sort' => $request->getQueryParams()['sort'],
+                    'top' => $request->getParsedBody()['top']
+                ];
+            })
+        );
+
+        $stub->method('bindArguments')->will(
+            $this->returnCallback(function ($params) {
+                return [$params['sort'], $params['top']];
+            })
+        );
+
         $this->assertEquals($stub->executeHandler($request), 'correct value');
     }
 
-    private function getRequest(array $params)
+    /**
+     * @dataProvider  requestProvider
+     * @depends clone testStub
+     */
+    public function testExecuteHandlerWithUri($request , $stub)
     {
-        $request_params = [
-            'REQUEST_URI' => $params['uri'],
-            'REQUEST_METHOD' => $params['method']
-        ];
+        $stub->param_sources = [];
 
-        $request = (new ServerRequest($request_params))
-                                ->withQueryParams(($params['query'] ?? []))
-                                ->withParsedBody(($params['parsed_body'] ?? []))
-                                ->withUploadedFiles(($params['uploaded_files'] ?? []));
+        $stub->handler = function ($user) {
+            if ($user->user_id == 55) {
+                return 'correct value';
+            } else {
+                return 'error value with uri';
+            }
+        };
 
-        return [$request];
+        $stub->method('resolveUriSource')->will(
+            $this->returnCallback(function ($request) {
+                return ['user' => 55];
+            })
+        );
+
+        $stub->method('bindArguments')->will(
+            $this->returnCallback(function ($params) {
+                return [$params['user']];
+            })
+        );
+
+        $this->assertEquals($stub->executeHandler($request), 'correct value');
+    }
+
+    /**
+     * @dataProvider  requestProvider
+     * @depends clone testStub
+     */
+    public function testExecuteHandlerWithRequest($request , $stub)
+    {
+        $stub->param_sources = [];
+
+        $stub->handler = function ($request) {
+            $correct_get = $request->getQueryParams()['sort'] == 22;
+            $correct_post = $request->getParsedBody()['top'] == 33;
+
+            return ($correct_get && $correct_post) ? 'correct value' : 'error value with request';
+        };
+
+        $stub->method('resolveUriSource')->will(
+            $this->returnCallback(function ($request) {
+                return [];
+            })
+        );
+
+        $stub->method('bindArguments')->will(
+            $this->returnCallback(function ($params) {
+                return [$params['request']];
+            })
+        );
+
+        $this->assertEquals($stub->executeHandler($request), 'correct value');
     }
 
     private function setResolveHandlerMethod($stub)
     {
         $stub->method('resolveHandler')->will(
-            $this->returnCallback(
-                function ($handler) {
-                    return $handler;
-                }
-            )
+            $this->returnCallback(function ($handler) {
+                return $handler;
+            })
         );
     }
-
-    private function setResolveSourcesMethod($stub)
-    {
-        $stub->method('resolveSources')->will(
-            $this->returnCallback(
-                function ($request) {
-                    $items['sort'] = $request->getQueryParams()['sort'];
-                    
-                    $items['top'] = $request->getParsedBody()['top'];
-                    
-                    return $items;
-                }
-            )
-        );
-    }
-
+    
     private function setHandleParams($stub)
     {
         $stub->method('handleParams')->will(
@@ -138,28 +185,27 @@ class RouteTraitTest extends PHPUnit_Framework_TestCase
         );
     }
 
-    private function setBindArguments($stub)
-    {
-        $stub->method('bindArguments')->will(
-            $this->returnCallback(
-                function ($params) {
-                    return [
-                        0 => $params['sort'] ?? null,
-                        1 => $params['top'] ?? null
-                    ];
-                }
-            )
-        );
-    }
-
     private function setCallHandler($stub)
     {
         $stub->method('callHandler')->will(
-            $this->returnCallback(
-                function ($handler, $arguments) {
-                    return $handler($arguments[0], $arguments[1]);
-                }
-            )
+            $this->returnCallback(function ($handler, $arguments) {
+                return call_user_func_array($handler, $arguments);
+            })
         );
+    }
+
+    private function getRequest(array $params)
+    {
+        $request_params = [
+            'REQUEST_URI' => $params['uri'],
+            'REQUEST_METHOD' => $params['method']
+        ];
+
+        $request = (new ServerRequest($request_params))
+                                ->withQueryParams(($params['query'] ?? []))
+                                ->withParsedBody(($params['parsed_body'] ?? []))
+                                ->withUploadedFiles(($params['uploaded_files'] ?? []));
+
+        return [$request];
     }
 }
